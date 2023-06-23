04ba359e21cd8bfee3bd65456c436dbe19a3725c
Bee <95414489+Bee-MC@users.noreply.github.com>
2023-06-23 17:04:15 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About Bee</title>
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+</head>
+<body>
+
+<h1>Work in progress</h1>
+<p>Yes.</p>
+
+</body>
+</html>
